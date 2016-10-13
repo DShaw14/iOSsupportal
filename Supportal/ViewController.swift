569135eb3976f5a487e1412e69fbe9a680d048ba
zmenken <zmenken@umich.edu>
2016-10-13 17:33:38 -0400

    Added app icon and added to the login page

--- Supportal/ViewController.swift
+++ Supportal/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+extension UIView {
+    
+    func addShadowView(width:CGFloat=0.2, height:CGFloat=0.2, Opacidade:Float=0.7, maskToBounds:Bool=false, radius:CGFloat=0.5){
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: width, height: height)
+        self.layer.shadowRadius = radius
+        self.layer.shadowOpacity = Opacidade
+        self.layer.masksToBounds = maskToBounds
+    }
+    
+}
+
+
+class TextField : UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        
+        let startingPoint   = CGPoint(x: rect.minX, y: rect.maxY)
+        let endingPoint     = CGPoint(x: rect.maxX, y: rect.maxY)
+        
+        let path = UIBezierPath()
+        
+        path.move(to: startingPoint)
+        path.addLine(to: endingPoint)
+        path.lineWidth = 2.0
+        
+        tintColor.setStroke()
+        
+        path.stroke()
+    }
+}
+
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton()
+        button.addShadowView()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
